debbf052f38ddee1bf3487bd16b17cb350d459f5
Юлия Старикова <89024321514y@gmail.com>
2022-09-27 16:12:53 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            button1.Top = rnd.Next(0, 300);
+            button1.Left = rnd.Next(0, 500);
+        }
+    }
+}
